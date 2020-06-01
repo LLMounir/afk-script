aad9cc3e8550c311e24347f57b281995165d294f
ml20 <mounirlalami@hotmail.com>
2020-06-01 05:09:01 +0200

    first commit

---
+++ afk.py
@@ -0,0 +1,17 @@
+﻿"""
+script pour afk;
+bouge la souris de 1 vers la droite toute les 10 secondes
+"""
+from pynput.mouse import Button, Controller
+import time
+import random
+
+time.sleep(10)
+mouse = Controller()
+i = 0
+while i< 300:
+  i += 1
+  print("Mouse clicked")
+  time.sleep(1)
+  mouse.press(Button.left)
+  mouse.release(Button.left)
